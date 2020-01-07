002a03e517efda4283cf61fbb1f64809c0d31771
Enrique Suárez Guette <36301561+Enriquesuarezgtte@users.noreply.github.com>
2020-01-07 09:53:15 -0500

    timeouts changes (#11)

--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -26,13 +26,8 @@ describe('Buy a t-shirt', () => {
   const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
   const orderResultPage: OrderResultPage = new OrderResultPage();
 
-  beforeEach(() => {
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 120000;
-  });
-
   it('then should be bought a t-shirt', async () => {
     await browser.get('http://automationpractice.com/');
-    await (browser.sleep(10000));
     await menuContentPage.goToTshirtMenu();
     await (browser.sleep(3000));
     await productListPage.addTshirtToCart();
