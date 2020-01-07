4c4bf65bef8f0d0b2e24c658819593f1e5a6c1a7
Enrique Suárez Guette <enriquesuarezgtte@gmail.com>
2020-01-07 10:50:14 -0500

    added implicit wait

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -13,6 +13,7 @@ export const config: Config = {
   },
   onPrepare: () => {
     browser.ignoreSynchronization = true;
+    browser.manage().timeouts().implicitlyWait(3000);
     reporter();
   },
   jasmineNodeOpts: {
