1df09f3be51efc18c1f32624999aa63e44073265
Enrique Suárez Guette <36301561+Enriquesuarezgtte@users.noreply.github.com>
2020-01-07 12:55:36 -0500

    added implicit wait (#12)

--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -29,31 +29,20 @@ describe('Buy a t-shirt', () => {
   it('then should be bought a t-shirt', async () => {
     await browser.get('http://automationpractice.com/');
     await menuContentPage.goToTshirtMenu();
-    await (browser.sleep(3000));
     await productListPage.addTshirtToCart();
-    await (browser.sleep(3000));
     await productAddedModalPage.goToShoppingCartSummary();
-    await (browser.sleep(3000));
 
     await orderSummary.goToLoginStep();
-    await (browser.sleep(3000));
     await signInCardStep.sendEmailandPasswKeys(emailKey, passwordKey);
     await signInCardStep.signIn();
-    await (browser.sleep(3000));
 
     await addressStepPage.goToShippingStep();
-    await (browser.sleep(3000));
 
     await shippingStepPage.acceptTermsOfService();
-    await (browser.sleep(3000));
 
     await shippingStepPage.goToPaymentStepPage();
-    await (browser.sleep(3000));
     await paymentStepPage.payByCheck();
-    await (browser.sleep(3000));
     await bankPaymentPage.confirmOrder();
-    await (browser.sleep(3000));
-
     await expect(orderResultPage.getOrderResult())
     .toBe('Your order on My Store is complete.');
 
